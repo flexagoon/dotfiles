1b0ec8a7331c4f8b52c51c8489ef3974e92f47f1
flexagoon <code@fxgn.dev>
2023-12-23 01:51:25 +0300

    vim: use proper xdg directories

--- dot_config/fish/conf.d/xdg.fish
+++ dot_config/fish/conf.d/xdg.fish
@@ -55,6 +55,9 @@ set -gx RUSTUP_HOME $XDG_DATA_HOME/rustup
 # SSH
 alias ssh "ssh -o UserKnownHostsFile=$XDG_DATA_HOME/ssh/known_hosts"
 
+# Vim
+set -gx VIMINIT 'let $MYVIMRC = !has("nvim") ? "$XDG_CONFIG_HOME/vim/vimrc" : "$XDG_CONFIG_HOME/nvim/init.vim" | so $MYVIMRC'
+
 # Wakatime
 set -gx WAKATIME_HOME $XDG_CONFIG_HOME/wakatime
 
