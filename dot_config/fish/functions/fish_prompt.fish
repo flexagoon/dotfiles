a8216cfab4bd01f63a6f7669b889753197ac0cbc
flexagoon <code@fxgn.dev>
2023-12-25 20:54:58 +0300

    fish: add a toolbx indicator

--- dot_config/fish/functions/fish_prompt.fish
+++ dot_config/fish/functions/fish_prompt.fish
@@ -3,6 +3,12 @@ function fish_prompt
     set -l pwd (string replace /var/home /home $PWD)
 
     echo
+
+    # Indicate if running inside of toolbx/distrobox
+    if test -e /run/.toolboxenv
+        echo -n "📦 "
+    end
+
     echo -s (set_color -o cyan)(prompt_pwd -D 3 $pwd) (set_color magenta)(fish_vcs_prompt)
     echo -ns (set_color green) "❯" (set_color normal) " "
 end
