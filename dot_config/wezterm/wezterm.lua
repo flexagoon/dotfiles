087a7afc3af5d563f5acd3f19c18eed1aadd7985
flexagoon <code@fxgn.dev>
2023-12-28 20:50:16 +0300

    wezterm: disable the default Alt+Enter binding

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -10,4 +10,13 @@ config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = true
 config.color_scheme = 'Catppuccin Frappe'
 
+config.keys = {
+  -- Alt+Enter is used for a line break in many REPLs
+  {
+    key = 'Enter',
+    mods = 'ALT',
+    action = wezterm.action.DisableDefaultAssignment,
+  },
+}
+
 return config
