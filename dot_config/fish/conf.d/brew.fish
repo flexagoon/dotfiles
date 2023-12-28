4b15685d96cea6ab35b7fd6d23aa948815ef855b
flexagoon <code@fxgn.dev>
2023-12-28 14:26:11 +0300

    fish: replace slow brew shellenv with manual configuration

--- dot_config/fish/conf.d/brew.fish
+++ dot_config/fish/conf.d/brew.fish
@@ -1,4 +1,10 @@
-eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv fish)
+set -gx HOMEBREW_PREFIX /home/linuxbrew/.linuxbrew
+set -gx HOMEBREW_CELLAR $HOMEBREW_PREFIX/Cellar
+set -gx HOMEBREW_REPOSITORY $HOMEBREW_PREFIX/Homebrew
+
+fish_add_path $HOMEBREW_PREFIX/bin $HOMEBREW_PREFIX/sbin
+set -gx MANPATH $HOMEBREW_PREFIX/share/man
+set -gx INFOPATH $HOMEBREW_PREFIX/share/info
 
 if test -d (brew --prefix)"/share/fish/completions"
     set -p fish_complete_path (brew --prefix)/share/fish/completions
