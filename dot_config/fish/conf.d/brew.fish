70132258311add9c326e4a618ccd27eddb6c2ed2
flexagoon <code@fxgn.dev>
2024-01-03 13:18:43 +0300

    fish: unset MANPATH and INFOPATH

--- dot_config/fish/conf.d/brew.fish
+++ dot_config/fish/conf.d/brew.fish
@@ -3,8 +3,6 @@ set -gx HOMEBREW_CELLAR $HOMEBREW_PREFIX/Cellar
 set -gx HOMEBREW_REPOSITORY $HOMEBREW_PREFIX/Homebrew
 
 fish_add_path $HOMEBREW_PREFIX/bin $HOMEBREW_PREFIX/sbin
-set -gx MANPATH $HOMEBREW_PREFIX/share/man
-set -gx INFOPATH $HOMEBREW_PREFIX/share/info
 
 if test -d (brew --prefix)"/share/fish/completions"
     set -p fish_complete_path (brew --prefix)/share/fish/completions
