261e04ae457a8387a71d87f83c6a8c73d8b02fa1
flexagoon <code@fxgn.dev>
2024-01-24 11:30:41 +0300

    wezterm: add key bindings for splitting and tab switching

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -24,6 +24,27 @@ config.keys = {
       args = { 'fish', '-c', 'julia' },
     },
   },
+
+  -- Better split keybinds
+  {
+    key = 's',
+    mods = 'ALT|SHIFT',
+    action = wezterm.action.SplitHorizontal {}
+  },
+  {
+    key = 'h', -- Wezterm's "vertical split" is actually horizontal
+    mods = 'ALT|SHIFT',
+    action = wezterm.action.SplitVertical {}
+  },
 }
+
+-- ALT + number to activate tab
+for i = 1, 9 do
+  table.insert(config.keys, {
+    key = tostring(i),
+    mods = 'ALT',
+    action = wezterm.action.ActivateTab(i - 1),
+  })
+end
 
 return config
