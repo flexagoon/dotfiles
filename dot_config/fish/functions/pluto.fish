a69751dc4367a1a8a22c8380aead6c4ebca7b13c
flexagoon <code@fxgn.dev>
2024-05-11 06:10:37 +0800

    fish: remove unneeded newline from the pluto function

--- dot_config/fish/functions/pluto.fish
+++ dot_config/fish/functions/pluto.fish
@@ -1,4 +1,3 @@
 function pluto --wraps='julia -e "import Pluto; Pluto.run()"' --description 'alias pluto julia -e "import Pluto; Pluto.run()"'
   julia -e "import Pluto; Pluto.run()" $argv
-        
 end
