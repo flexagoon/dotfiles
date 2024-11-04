80c49b180d7614ab28bcdbbfbeee3a85d24c36ae
flexagoon <code@fxgn.dev>
2024-11-04 03:02:29 +0300

    xdg: add more tools

--- dot_config/fish/conf.d/xdg.fish
+++ dot_config/fish/conf.d/xdg.fish
@@ -5,12 +5,15 @@ set -gx XDG_CONFIG_HOME ~/.config
 set -gx XDG_STATE_HOME ~/.local/state
 
 # Android
-set -gx ANDROID_HOME $ANDROID_USER_HOME/sdk
+set -gx ANDROID_HOME $ANDROID_USER_HOME
 set -gx ANDROID_USER_HOME $XDG_DATA_HOME/android
 alias adb "HOME=$ANDROID_USER_HOME command adb"
 
 # Bash
 set -gx HISTFILE $XDG_STATE_HOME/bash/history
+
+# Elixir
+set -gx MIX_HOME $XDG_DATA_HOME/mix
 
 # Fly
 set -gx FLY_CONFIG_DIR $XDG_STATE_HOME/fly
@@ -42,9 +45,18 @@ set -gx JULIAUP_DEPOT_PATH $XDG_DATA_HOME/julia
 # Mathematica
 set -gx MATHEMATICA_USERBASE $XDG_CONFIG_HOME/mathematica
 
+# MyPy
+set -gx MYPY_CACHE_DIR $XDG_CACHE_HOME/mypy
+
+# MySQL
+set -gx MYSQL_HISTFILE $XDG_DATA_HOME/mysql_history
+
 # Node.js
 set -gx NPM_CONFIG_USERCONFIG $XDG_CONFIG_HOME/npm/npmrc
 set -gx NODE_REPL_HISTORY $XDG_STATE_HOME/node_repl_history
+
+# pnpm
+set -gx PNPM_HOME $XDG_DATA_HOME/pnpm
 
 # PostgreSQL
 set -gx PSQL_HISTORY $XDG_DATA_HOME/psql_history
@@ -58,7 +70,7 @@ set -gx CARGO_HOME $XDG_DATA_HOME/cargo
 set -gx RUSTUP_HOME $XDG_DATA_HOME/rustup
 
 # SSH
-alias ssh "ssh -o UserKnownHostsFile=$XDG_DATA_HOME/ssh/known_hosts -i $XDG_DATA_HOME/ssh/id_rsa"
+alias ssh "ssh -o UserKnownHostsFile=$XDG_DATA_HOME/ssh/known_hosts -i $XDG_DATA_HOME/ssh/id_ed25519"
 
 # SQLite
 set -gx SQLITE_HISTORY $XDG_CACHE_HOME/sqlite_history
