bf02f23f5c6f7bc3a84086648a265d5b943ac226
flexagoon <code@fxgn.dev>
2024-11-04 03:04:32 +0300

    fish: use a custom script instead of cookiecutter

--- dot_config/fish/functions/new.fish
+++ dot_config/fish/functions/new.fish
@@ -1,3 +1,31 @@
-function new --wraps='cookiecutter https://github.com/flexagoon/cookies --directory' --description 'Create a new project from a template'
-  cookiecutter https://github.com/flexagoon/cookies --directory $argv
+# A simple project templating tool. Basic alternative to cookiecutter, copier, etc.
+# I had to write this since Cookiecutter doesn't support XDG directories and puts
+# shit in my $HOME, and Copier doesn't allow having multiple templates in the same
+# repository.
+function new  --description 'Create a new project from a template'
+  if test (count $argv) -lt 2
+    echo 'Usage: new <template> <name>'
+    return 1
+  end
+
+  set -l cookies $XDG_DATA_HOME/cookies
+
+  if test ! -d $cookies
+    git clone https://github.com/flexagoon/cookies $XDG_DATA_HOME/cookies
+  else
+    env -C $cookies git pull > /dev/null
+  end
+
+  set -l template $argv[1]
+  set -l name $argv[2]
+
+  cp -r $cookies/$template $name
+  cd $name
+
+  git init > /dev/null
+  git remote add origin https://github.com/(git config user.name)/$name
+
+  if test -f _hooks.fish
+    fish _hooks.fish && rm _hooks.fish
+  end
 end
