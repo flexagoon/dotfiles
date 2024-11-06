7e6f86061ff27377cb72c58daade8470a7a779ae
flexagoon <code@fxgn.dev>
2024-11-06 12:08:18 +0300

    autorestic: unlock before running

--- .chezmoiscripts/run_once_after_services.sh
+++ .chezmoiscripts/run_once_after_services.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 systemctl enable --user --now kmonad
 systemctl enable --user --now awatcher
-systemctl enable --user --now autorestic.timer
+systemctl enable --user --now backup.timer
 systemctl enable --user --now clean-downloads.timer
