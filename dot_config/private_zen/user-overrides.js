d2aac9903ed4c19ece57e1e7d5616c7b63b37557
flexagoon <code@fxgn.dev>
2024-11-16 15:36:49 +0300

    zen: move from firefox to zen browser

--- dot_config/private_zen/user-overrides.js
+++ dot_config/private_zen/user-overrides.js
@@ -11,12 +11,6 @@ user_pref("browser.download.useDownloadDir", true);
 // Don't clear history
 user_pref("privacy.clearOnShutdown.history", false);
 user_pref("privacy.clearOnShutdown_v2.historyFormDataAndDownloads", false);
-// Enable DRM
-user_pref("media.eme.enabled", true);
-// Disable letterboxing
-user_pref("privacy.resistFingerprinting.letterboxing", false);
-// Enable WebGL
-user_pref("webgl.disabled", false);
 
 // REMOVE USELESS FEATURES
 // -----------------------------------------------
@@ -27,16 +21,3 @@ user_pref("privacy.userContext.enabled", false);
 user_pref("privacy.userContext.ui.enabled", false);
 // Don't save passwords
 user_pref("signon.rememberSignons", false);
-
-// USE FIREFOX-GNOME-THEME
-// -----------------------------------------------
-// Enable customChrome.css
-user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
-// Set UI density to normal
-user_pref("browser.uidensity", 0);
-// Enable SVG context-propertes
-user_pref("svg.context-properties.content.enabled", true);
-// Disable private window dark theme
-user_pref("browser.theme.dark-private-windows", false);
-// Hide single tab
-user_pref("gnomeTheme.hideSingleTab", true);
