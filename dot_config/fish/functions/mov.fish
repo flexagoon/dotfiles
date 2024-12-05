5c09e49edfdfe5916fe101b6e1a8dfafe184dbdb
flexagoon <code@fxgn.dev>
2024-12-06 02:15:36 +0300

    fish: format the mov functon

--- dot_config/fish/functions/mov.fish
+++ dot_config/fish/functions/mov.fish
@@ -1,3 +1,8 @@
 function mov --description 'Convert video to DNxHD mov'
-  ffmpeg -hwaccel cuda -i $argv[1] -c:v dnxhd -profile:v dnxhr_hq -c:a pcm_s16le -pix_fmt yuv422p $argv[2..]; 
+  ffmpeg -hwaccel cuda \
+         -c:v dnxhd \
+         -profile:v dnxhr_hq \
+         -c:a pcm_s16le \
+         -pix_fmt yuv422p \
+         -i $argv[1] $argv[2..]; 
 end
