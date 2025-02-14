7f0e864d0bb9985e9f9228b11830e89cdce90cfb
flexagoon <code@fxgn.dev>
2025-02-15 02:36:28 +0300

    fish: remove the cryptenroll abbreviation

--- dot_config/fish/conf.d/abbreviations.fish
+++ dot_config/fish/conf.d/abbreviations.fish
@@ -14,5 +14,3 @@ abbr  td   "cd (mktemp -d)"
 abbr  tmp  "cd (mktemp -d)"
 abbr  j    julia
 abbr  c    chezmoi
-
-abbr  cr   sudo systemd-cryptenroll --wipe-slot=tpm2 --tpm2-device=auto --tpm2-pcrs=1+4+5+7+9 /dev/nvme0n1p3
