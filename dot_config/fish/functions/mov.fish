ca05a570f3f8da8cbd5a59f50e8b26fd74b773e8
flexagoon <code@fxgn.dev>
2025-05-22 23:33:18 +0300

    fish: fix the argument order in the mov function

--- dot_config/fish/functions/mov.fish
+++ dot_config/fish/functions/mov.fish
@@ -1,8 +1,9 @@
 function mov --description 'Convert video to DNxHD mov'
   ffmpeg -hwaccel cuda \
+         -i $argv[1] \
          -c:v dnxhd \
          -profile:v dnxhr_hq \
          -c:a pcm_s16le \
          -pix_fmt yuv422p \
-         -i $argv[1] $argv[2..]; 
+         $argv[2..]; 
 end
