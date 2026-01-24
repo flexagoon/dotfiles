5da5f6639b92cd963748d1334363500bf08bf440
flexagoon <code@fxgn.dev>
2026-01-24 02:09:19 +0100

    fish: rename esp function to idf.py

--- dot_config/fish/functions/idf.py.fish
+++ dot_config/fish/functions/idf.py.fish
@@ -1,8 +1,11 @@
-function esp --description 'Activate ESP environment'
+function idf.py --description 'Run idf.py for the first time'
   set IDF_PATH ~/Documents/Git/esp-idf
 
   set -gx IDF_TOOLCHAIN clang
   set -gx IDF_TARGET esp32c3
 
   source $IDF_PATH/export.fish
+
+	functions -e idf.py
+	idf.py $argv
 end
